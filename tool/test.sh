6299903beaa66659e3ff8701d5a9716711715c22
Dao Hoang Son <daohoangson@gmail.com>
2019-03-31 10:10:19 +0700

    Add support for test coverage

--- tool/test.sh
+++ tool/test.sh
@@ -2,10 +2,15 @@
 
 set -e
 
+_testArgs=''
+if [ ! -z "$COVERAGE" ]; then
+  _testArgs="${_testArgs} --coverage"
+fi
+
 cd "$( dirname $( dirname ${BASH_SOURCE[0]}))"/packages/core
-flutter test
+flutter test $( echo $_testArgs )
 echo 'flutter_widget_from_html_core OK'
 
 cd ../..
-flutter test
+flutter test $( echo $_testArgs )
 echo 'flutter_widget_from_html OK'
