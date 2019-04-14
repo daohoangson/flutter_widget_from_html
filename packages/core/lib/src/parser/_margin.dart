6b107f3c7adaa9661983c160e80c1fea03b80cb9
Dao Hoang Son <daohoangson@gmail.com>
2019-04-14 18:49:52 +0700

    Fix margin with partial invalid values.
    Related to #21

--- packages/core/lib/src/parser/_margin.dart
+++ packages/core/lib/src/parser/_margin.dart
@@ -10,27 +10,32 @@ final _kMarginValuesFourRegex =
     RegExp(r'^([^\s]+)\s+([^\s]+)\s+([^\s]+)\s+([^\s]+)$');
 final _kMarginValuesTwoRegex = RegExp(r'^([^\s]+)\s+([^\s]+)$');
 
+double _parseMarginValue(String str) {
+  final d = _unitParseValue(str);
+  return (d == null || d < 0) ? 0 : d;
+}
+
 EdgeInsetsGeometry _marginParseAll(String value) {
   final valuesFour = _kMarginValuesFourRegex.firstMatch(value);
   if (valuesFour != null) {
-    final t = _unitParseValue(valuesFour.group(1));
-    final r = _unitParseValue(valuesFour.group(2));
-    final b = _unitParseValue(valuesFour.group(3));
-    final l = _unitParseValue(valuesFour.group(4));
-    if (t == null && r == null && b == null && l == null) return null;
+    final t = _parseMarginValue(valuesFour.group(1));
+    final r = _parseMarginValue(valuesFour.group(2));
+    final b = _parseMarginValue(valuesFour.group(3));
+    final l = _parseMarginValue(valuesFour.group(4));
+    if (t == 0 && r == 0 && b == 0 && l == 0) return null;
     return EdgeInsets.fromLTRB(l, t, r, b);
   }
 
   final valuesTwo = _kMarginValuesTwoRegex.firstMatch(value);
   if (valuesTwo != null) {
-    final v = _unitParseValue(valuesTwo.group(1));
-    final h = _unitParseValue(valuesTwo.group(2));
-    if (v == null && h == null) return null;
+    final v = _parseMarginValue(valuesTwo.group(1));
+    final h = _parseMarginValue(valuesTwo.group(2));
+    if (v == 0 && h == 0) return null;
     return EdgeInsets.symmetric(horizontal: h, vertical: v);
   }
 
-  final all = _unitParseValue(value);
-  return all == null ? null : EdgeInsets.all(all);
+  final all = _parseMarginValue(value);
+  return all == 0 ? null : EdgeInsets.all(all);
 }
 
 EdgeInsetsGeometry _marginParseOne(
