0ecf4207dfc1cc2048e32672c26963bfa2a0d43f
Dao Hoang Son <daohoangson@gmail.com>
2019-04-27 08:43:19 +0700

    Add test for margin inside A tag

--- test/tag_a_test.dart
+++ test/tag_a_test.dart
@@ -4,7 +4,6 @@ import '_.dart';
 
 const kColor = '#FF0000FF';
 const kHref = 'http://domain.com/href';
-const kImgSrc = 'http://domain.com/image.png';
 
 void main() {
   testWidgets('renders underline', (WidgetTester tester) async {
@@ -32,27 +31,5 @@ void main() {
       explained,
       equals('[GestureDetector:child=[RichText:($kColor+u:Foo)]]'),
     );
-  });
-
-  testWidgets('renders DIV tags inside', (WidgetTester tester) async {
-    final html = '<a href="$kHref"><div>Foo</div><div>Bar</div></a>';
-    final explained = await explain(tester, html);
-    expect(
-      explained,
-      equals(
-        '[Column:children='
-        '[GestureDetector:child=[RichText:($kColor+u:Foo)]],'
-        '[GestureDetector:child=[RichText:($kColor+u:Bar)]]]',
-      ),
-    );
-  });
-
-  testWidgets('renders IMG tag inside', (WidgetTester tester) async {
-    final html = '<a href="$kHref"><img src="$kImgSrc" /></a>';
-    final explained = await explain(tester, html);
-    expect(
-        explained,
-        equals('[GestureDetector:child='
-            '[CachedNetworkImage:http://domain.com/image.png]]'));
   });
 }
