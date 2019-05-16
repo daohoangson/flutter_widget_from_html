adc1fb4b2181a2550be04f1b0fcb6114b1b0548d
Đào Hoàng Sơn <daohoangson@gmail.com>
2019-05-16 10:38:16 +0700

    Write `webView` config info

--- lib/src/html_widget.dart
+++ lib/src/html_widget.dart
@@ -6,7 +6,27 @@ import 'config.dart';
 import 'widget_factory.dart' as extended;
 
 class HtmlWidget extends core.HtmlWidget implements Config {
+
+  /// Flag to render WebView for IFRAME tag.
+  ///
+  /// You must perform additional configuration for this to work.
+  /// ### iOS
+  /// Add this at the end of `ios/Runner/Info.plist` to enable
+  /// Flutter's experimental platform view.
+  /// See more info [here](https://pub.dev/packages/webview_flutter#ios).
+  /// ```plist
+  /// <key>io.flutter.embedded_views_preview</key>
+  /// <string>YES</string>
+  /// ```
+  /// ### Android
+  /// Add this into `android/app/src/main/AndroidManifest.xml`
+  /// to enable internet access. Without this, you will most likely
+  /// see a `net:ERR_CACHE_MISS` error for each iframe being rendered.
+  /// ```xml
+  /// <uses-permission android:name="android.permission.INTERNET" />
+  /// ```
   final bool webView;
+
   final bool webViewJs;
 
   const HtmlWidget(
