b66f2afdaf8b58395ce411a8e1c835c3e28bc50a
Dao Hoang Son <daohoangson@gmail.com>
2019-05-17 09:07:21 +0700

    Add doc for `widget0`

--- packages/core/lib/src/core_helpers.dart
+++ packages/core/lib/src/core_helpers.dart
@@ -2,9 +2,10 @@ import 'package:flutter/widgets.dart';
 import 'core_widget_factory.dart';
 import 'data_classes.dart';
 
-part 'ops/container.dart';
 part 'ops/wrap.dart';
-
 part 'parser/border.dart';
 part 'parser/color.dart';
 part 'parser/css.dart';
+
+/// A no op placeholder widget.
+const widget0 = const SizedBox.shrink();
