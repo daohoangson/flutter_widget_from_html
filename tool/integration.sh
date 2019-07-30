1db482b1425c722210d7590a184e92e2e52f2108
Dao Hoang Son <daohoangson@gmail.com>
2019-07-30 15:52:50 +0700

    Revert "Include video_player.dart in integration test"
    This reverts commit cb4516865ec5fc10b7cdee99fce6703bf0c32f2d.

--- tool/integration.sh
+++ tool/integration.sh
@@ -3,9 +3,5 @@
 set -e
 
 cd "$( dirname $( dirname ${BASH_SOURCE[0]}))"/packages/example
-
-flutter drive --target=test_driver/video_player.dart
-echo 'test_driver/video_player.dart OK'
-
 flutter drive --target=test_driver/web_view.dart
 echo 'test_driver/web_view.dart OK'
