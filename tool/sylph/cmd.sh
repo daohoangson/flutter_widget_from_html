aca39d0ef7ab1bc513f03ea619bd55d62738fcae
Đào Hoàng Sơn <daohoangson@gmail.com>
2019-10-31 15:55:41 +0700

    Improve integration tests (#110)

--- tool/sylph/cmd.sh
+++ tool/sylph/cmd.sh
@@ -3,6 +3,9 @@
 set -e
 cd $( dirname $( dirname $( dirname ${BASH_SOURCE[0]})))
 
+sudo apt-get update
+sudo apt-get install -y python zip
+
 # Install AWS Command Line Interface (AWS CLI)
 if [ ! -f awscli-bundle.zip ]; then
   curl "https://s3.amazonaws.com/aws-cli/awscli-bundle.zip" -o "awscli-bundle.zip"
@@ -16,11 +19,7 @@ fi
 
 # https://github.com/mmcc007/sylph
 export PATH="$PATH":"$HOME/.pub-cache/bin"
-pub global activate sylph
-
-# Others
-sudo apt-get update
-sudo apt-get install -y zip
+pub global activate --source git https://github.com/daohoangson/sylph.git
 
 cd packages/example
 flutter pub get
