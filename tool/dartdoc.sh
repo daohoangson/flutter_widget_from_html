0f01efc9cde0f35eda6d331837fbcf09833399ff
Dao Hoang Son <daohoangson@gmail.com>
2019-12-30 14:24:23 +0700

    Deploy dartdoc on `now.sh`

--- tool/dartdoc.sh
+++ tool/dartdoc.sh
@@ -2,9 +2,21 @@
 
 set -e
 
-cd "$( dirname $( dirname ${BASH_SOURCE[0]}))"
-
 # https://github.com/dart-lang/dartdoc/issues/1431
-_exclude=dart:ui,dart:async,dart:collection,dart:convert,dart:core,dart:developer,dart:math,dart:typed_data,dart:io,dart:isolate
+_exclude=
+_exclude=$_exclude,dart:async
+_exclude=$_exclude,dart:collection
+_exclude=$_exclude,dart:convert
+_exclude=$_exclude,dart:core
+_exclude=$_exclude,dart:developer
+_exclude=$_exclude,dart:ffi
+_exclude=$_exclude,dart:html
+_exclude=$_exclude,dart:io
+_exclude=$_exclude,dart:isolate
+_exclude=$_exclude,dart:js
+_exclude=$_exclude,dart:js_util
+_exclude=$_exclude,dart:math
+_exclude=$_exclude,dart:typed_data
+_exclude=$_exclude,dart:ui
 
 exec dartdoc --exclude $_exclude
