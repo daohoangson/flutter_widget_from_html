9e334798a8866021426c105fdbe5d6aeb9ff6409
Dao Hoang Son <daohoangson@gmail.com>
2020-05-20 09:56:54 +0700

    Revert "Render source for debugging"
    This reverts commit 5d420efc2b10a8e42e3540171e080f667b4abbbc.

--- api/iframe.ts
+++ api/iframe.ts
@@ -5,11 +5,6 @@ export default (request: NowRequest, response: NowResponse) => {
   if (!body) {
     return response.status(400).send('POST param `body` or include in query string to render.')
   }
-  
-  const escaped = body.replace(/&/g, '&amp;')
-    .replace(/"/g, '&quot;')
-    .replace(/</g, '&lt;')
-    .replace(/>/g, '&gt;')
 
   const html = `
 <!doctype html>
@@ -18,10 +13,7 @@ export default (request: NowRequest, response: NowResponse) => {
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width">
 </head>
-<body>
-  <pre class="source">${escaped}</pre>
-  <div class="render">${body}</div>
-</body>
+<body>${body}</body>
 </html>
 `
 
