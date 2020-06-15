e1cf61cfe9ed5596ee4dc2af7d27973a4524139a
Dao Hoang Son <daohoangson@gmail.com>
2020-06-15 09:01:43 +0700

    Re-organize node files

--- tool/node.sh
+++ tool/node.sh
@@ -6,5 +6,7 @@ cd "$( dirname $( dirname ${BASH_SOURCE[0]}))"
 _pwd=$( pwd )
 
 exec docker run --rm -it \
+  -p 3000:3000 \
   -v "$_pwd:/project" -w /project \
+  -v "$_pwd/.node/root/.npm:/root/.npm" \
   node:lts /bin/bash
