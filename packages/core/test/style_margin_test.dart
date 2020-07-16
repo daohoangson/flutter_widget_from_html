9d9e8f03118e614a0a97a83779a40541b5448977
Dao Hoang Son <daohoangson@gmail.com>
2020-07-16 23:19:59 +0700

    Move the remaining tests from `padding_test` into `style_margin_test`

--- packages/core/test/style_margin_test.dart
+++ packages/core/test/style_margin_test.dart
@@ -340,6 +340,31 @@ void main() {
     });
   });
 
+  group('trimming', () {
+    testWidgets('trims top intances', (WidgetTester tester) async {
+      final html = '<div style="margin-top: 1em">Foo</div>';
+      final explained = await helper.explain(tester, html);
+      expect(explained, equals('[RichText:(:Foo)]'));
+    });
+
+    testWidgets('trims bottom instances', (WidgetTester tester) async {
+      final html = '<div style="margin-bottom: 1em">Foo</div>';
+      final explained = await helper.explain(tester, html);
+      expect(explained, equals('[RichText:(:Foo)]'));
+    });
+
+    testWidgets('trims both ways', (WidgetTester tester) async {
+      final html = '<div style="margin: 1em 0">Foo</div>'
+          '<div style="margin: 1em 0">Bar</div>';
+      final explained = await helper.explain(tester, html);
+      expect(
+          explained,
+          equals('[Column:children=[RichText:(:Foo)],'
+              '[SizedBox:0.0x10.0],'
+              '[RichText:(:Bar)]]'));
+    });
+  });
+
   group('invalid values', () {
     testWidgets('4 values', (WidgetTester tester) async {
       final html = '<div style="margin: a b c d">Foo</div>';
