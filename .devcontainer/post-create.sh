9245d639d744bd45b11b9b9fa3f3d6eb173729cf
Đào Hoàng Sơn <daohoangson@gmail.com>
2020-07-18 10:35:00 +0700

    Update post-create.sh

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -9,8 +9,8 @@ _baseUrl=$( cat /tmp/releases.json | jq -r .base_url )
 _stableHash=$( cat /tmp/releases.json | jq -r .current_release.stable )
 _stableArchive=$( cat /tmp/releases.json | jq -r ".releases[] | select(.hash == \"$_stableHash\") | .archive" )
 curl $_baseUrl/$_stableArchive -vo /tmp/flutter.tar.xz
-tar xf /tmp/flutter.tar.xz
-mv /tmp/flutter $HOME/flutter
+tar -xf /tmp/flutter.tar.xz
+mv ./flutter $HOME/flutter
 echo 'PATH=$PATH:$HOME/flutter/bin' >> $HOME/.profile
 
 # pub get
