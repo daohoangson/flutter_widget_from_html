38f59fd3eddaf0fb90a6e8f6961d0bf2ae88c12a
Đào Hoàng Sơn <daohoangson@gmail.com>
2020-07-18 09:12:43 +0700

    Create post-create.sh

---
+++ .devcontainer/post-create.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+set -e
+
+# Install latest Flutter stable
+sudo apt-get install -y jq
+curl https://storage.googleapis.com/flutter_infra/releases/releases_linux.json -Lo /tmp/releases.json
+_baseUrl=$( cat /tmp/releases.json | jq -r .base_url )
+_stableHash=$( cat /tmp/releases.json | jq -r .current_release.stable )
+_stableArchive=$( cat /tmp/releases.json | jq -r ".releases[] | select(.hash == \"$_stableHash\") | .archive" )
+curl $_baseUrl/$_stableArchive -vo flutter.zip
+unzip -qq flutter.zip
+mv flutter $HOME/flutter
+echo 'PATH=$PATH:$HOME/flutter/bin' >> $HOME/.profile
