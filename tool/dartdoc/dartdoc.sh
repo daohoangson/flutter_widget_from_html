b7f6e6959cf8f44633d8daccc082222fb12642f9
Dao Hoang Son <daohoangson@gmail.com>
2020-07-20 01:47:36 +0700

    Add dartdoc job

--- tool/dartdoc/dartdoc.sh
+++ tool/dartdoc/dartdoc.sh
@@ -2,21 +2,10 @@
 
 set -e
 
-# https://github.com/dart-lang/dartdoc/issues/1431
-_exclude=
-_exclude=$_exclude,dart:async
-_exclude=$_exclude,dart:collection
-_exclude=$_exclude,dart:convert
-_exclude=$_exclude,dart:core
-_exclude=$_exclude,dart:developer
-_exclude=$_exclude,dart:ffi
-_exclude=$_exclude,dart:html
-_exclude=$_exclude,dart:io
-_exclude=$_exclude,dart:isolate
-_exclude=$_exclude,dart:js
-_exclude=$_exclude,dart:js_util
-_exclude=$_exclude,dart:math
-_exclude=$_exclude,dart:typed_data
-_exclude=$_exclude,dart:ui
+flutter pub get
 
-exec dartdoc --exclude $_exclude
+# https://github.com/dart-lang/dartdoc/issues/1949
+export FLUTTER_ROOT=$( dirname $( dirname $( which flutter ) ) )
+$FLUTTER_ROOT/bin/cache/dart-sdk/bin/pub global activate dartdoc
+
+exec $FLUTTER_ROOT/bin/cache/dart-sdk/bin/pub global run dartdoc
