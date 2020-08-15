5fdd929910b80df3592ff8912dbcc21441ec037b
Dao Hoang Son <daohoangson@gmail.com>
2020-08-15 22:48:06 +0700

    Add document for `ImageMetadata` and `ImageSource`

--- packages/core/lib/src/core_data.dart
+++ packages/core/lib/src/core_data.dart
@@ -287,25 +287,35 @@ class HtmlWidgetDependency<T> {
   HtmlWidgetDependency(this.value);
 }
 
+/// An image.
 @immutable
 class ImageMetadata {
+  /// The image alternative text.
   final String alt;
 
+  /// The image sources.
   final Iterable<ImageSource> sources;
 
+  /// The image title.
   final String title;
 
+  /// Creates an image.
   ImageMetadata({this.alt, this.sources, this.title});
 }
 
+/// An image source.
 @immutable
 class ImageSource {
+  /// The image height.
   final double height;
 
+  /// The image URL.
   final String url;
 
+  /// The image width.
   final double width;
 
+  /// Creates a source.
   ImageSource(this.url, {this.height, this.width}) : assert(url != null);
 }
 
