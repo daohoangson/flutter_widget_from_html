2d6abdc57c618bfe83521cf6c20dfbea8a99e441
Dao Hoang Son <daohoangson@gmail.com>
2020-09-15 22:40:10 +0700

    Include `.packages` during test runs

--- tool/test.sh
+++ tool/test.sh
@@ -4,18 +4,21 @@ set -e
 
 ( \
   cd ./packages/core \
+  && flutter pub get && cat .packages \
   && flutter test "$@" \
   && echo 'packages/core OK' \
 )
 
 ( \
   cd ./packages/enhanced \
+  && flutter pub get && cat .packages \
   && flutter test "$@" \
   && echo 'packages/enhanced OK' \
 )
 
 ( \
   cd ./demo_app \
+  && flutter pub get && cat .packages \
   && flutter test "$@" \
   && echo 'demo_app OK' \
 )
