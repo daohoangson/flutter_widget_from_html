29c1aded1c26f0f29853004bbb7e17bf959c2f25
Dao Hoang Son <daohoangson@gmail.com>
2021-02-26 15:36:08 +0700

    Override `WidgetFactory.onTapUrl` instead of `.gestureTapCallback`

--- packages/fwfh_url_launcher/lib/src/url_launcher_factory.dart
+++ packages/fwfh_url_launcher/lib/src/url_launcher_factory.dart
@@ -1,23 +1,16 @@
-import 'package:flutter/widgets.dart';
 import 'package:flutter_widget_from_html_core/flutter_widget_from_html_core.dart';
 import 'package:url_launcher/url_launcher.dart';
 
 /// A mixin that can launch A tag via `url_launcher` plugin.
 mixin UrlLauncherFactory on WidgetFactory {
   @override
-  GestureTapCallback? gestureTapCallback(String url) {
-    final callback = super.gestureTapCallback(url);
-    if (callback == null) return null;
-    if (!url.startsWith('http')) return callback;
+  Future<bool> onTapUrl(String url) async {
+    final result = await super.onTapUrl(url);
+    if (result) return result;
 
-    return () async {
-      final ok = await canLaunch(url);
-      if (ok) {
-        await launch(url);
-        return;
-      }
+    final ok = await canLaunch(url);
+    if (!ok) return false;
 
-      callback();
-    };
+    return launch(url);
   }
 }
