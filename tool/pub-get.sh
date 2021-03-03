dbeabace394d62890db9f9328994a1c28b9eb862
Dao Hoang Son <daohoangson@gmail.com>
2021-03-03 22:56:28 +0700

    Update pub-get.sh

--- tool/pub-get.sh
+++ tool/pub-get.sh
@@ -2,6 +2,12 @@
 
 set -e
 
+find . -name 'pubspec.lock' -delete
+
 ( cd packages/core && flutter pub get )
+( cd packages/fwfh_cached_network_image && flutter pub get )
+( cd packages/fwfh_chewie && flutter pub get )
+( cd packages/fwfh_svg && flutter pub get )
+( cd packages/fwfh_webview && flutter pub get )
 ( cd packages/enhanced && flutter pub get )
 ( cd demo_app && flutter pub get )
