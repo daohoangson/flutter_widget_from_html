3ad7b00cd2175cfc8f7187ee30180c1f8c0ce2a8
Dao Hoang Son <daohoangson@gmail.com>
2021-04-05 09:01:08 +0700

    Include `fwfh_url_launcher` in `pub-get.sh`

--- tool/pub-get.sh
+++ tool/pub-get.sh
@@ -8,6 +8,7 @@ find . -name 'pubspec.lock' -delete
 ( cd packages/fwfh_cached_network_image && flutter pub get )
 ( cd packages/fwfh_chewie && flutter pub get )
 ( cd packages/fwfh_svg && flutter pub get )
+( cd packages/fwfh_url_launcher && flutter pub get )
 ( cd packages/fwfh_webview && flutter pub get )
 ( cd packages/enhanced && flutter pub get )
 ( cd demo_app && flutter pub get )
