f57882394805faa647e6d84f944f668c697a7361
Dao Hoang Son <daohoangson@gmail.com>
2021-04-28 09:12:21 +0700

    Remove unused imports

--- packages/core/test/tag_a_test.dart
+++ packages/core/test/tag_a_test.dart
@@ -1,6 +1,4 @@
-import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:flutter_widget_from_html_core/flutter_widget_from_html_core.dart';
 import 'package:mocktail_image_network/mocktail_image_network.dart';
 
 import '_.dart';
