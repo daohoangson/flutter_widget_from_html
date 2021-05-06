94110ec0e0b4711d948fd0a95e83ca2eb98a8483
Dao Hoang Son <daohoangson@gmail.com>
2021-05-06 15:55:35 +0700

    Avoid ignoring `undefined_prefixed_name` lint

---
+++ packages/fwfh_webview/lib/src/external/dart_ui_fallback.dart
@@ -0,0 +1,6 @@
+class platformViewRegistry {
+  /// TODO: remove this when it's no longer needed
+  /// See https://github.com/flutter/flutter/issues/55000
+  static void registerViewFactory(
+      String viewTypeId, dynamic Function(int viewId) viewFactory) {}
+}
