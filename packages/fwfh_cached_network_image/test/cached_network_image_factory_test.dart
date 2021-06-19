3fb99cb9b687016b25bd8e7be333f013281e9937
Dao Hoang Son <daohoangson@gmail.com>
2021-06-20 01:08:13 +0700

    Update fwfh_cached_network_image

--- packages/fwfh_cached_network_image/test/cached_network_image_factory_test.dart
+++ packages/fwfh_cached_network_image/test/cached_network_image_factory_test.dart
@@ -23,9 +23,9 @@ class _WidgetFactory extends WidgetFactory with CachedNetworkImageFactory {}
 
 void main() {
   testWidgets('renders IMG tag', (WidgetTester tester) async {
-    final sizingConstraints = 'height≥0.0,height=auto,width≥0.0,width=auto';
-    final src = 'http://domain.com/image.png';
-    final html = '<img src="$src" />';
+    const sizingConstraints = 'height≥0.0,height=auto,width≥0.0,width=auto';
+    const src = 'http://domain.com/image.png';
+    const html = '<img src="$src" />';
     final explained = await explain(tester, html);
     expect(
       explained,
