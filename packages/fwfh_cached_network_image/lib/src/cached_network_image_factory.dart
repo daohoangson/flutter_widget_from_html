1b59963cfe1be2a714720ab48b73e0d13055542b
Daniel Felten <daniel.felten@trimexa.de>
2021-07-04 18:36:17 +0200

    Overwrite buildImageWidget in CachedNetworkImageFactory for placeholder functionality

--- packages/fwfh_cached_network_image/lib/src/cached_network_image_factory.dart
+++ packages/fwfh_cached_network_image/lib/src/cached_network_image_factory.dart
@@ -5,6 +5,28 @@ import 'package:flutter_widget_from_html_core/flutter_widget_from_html_core.dart
 /// A mixin that can render IMG with `cached_network_image` plugin.
 mixin CachedNetworkImageFactory on WidgetFactory {
   @override
-  ImageProvider? imageProviderFromNetwork(String url) =>
-      url.isNotEmpty ? CachedNetworkImageProvider(url) : null;
+  Widget? buildImageWidget(BuildMetadata meta, ImageSource src) {
+    final url = src.url;
+
+    if (url.isEmpty) return null;
+
+    // Use default image loading for assets, data images and files
+    if (url.startsWith('asset:') ||
+        url.startsWith('data:image/') ||
+        url.startsWith('file:')) {
+      return super.buildImageWidget(meta, src);
+    }
+
+    return CachedNetworkImage(
+      imageUrl: url,
+      fit: BoxFit.fill,
+      placeholder: (context, url) => imageLoadingBuilder(
+        context,
+        Container(),
+        ImageChunkEvent(cumulativeBytesLoaded: 1, expectedTotalBytes: 1),
+        src,
+      ),
+      errorWidget: (_1, _2, _3) => imageErrorBuilder(_1, _3, null, src),
+    );
+  }
 }
