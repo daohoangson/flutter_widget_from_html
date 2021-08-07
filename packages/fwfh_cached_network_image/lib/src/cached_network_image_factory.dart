fd2c4b126d663bb795711a768dedb1de2d4e0d05
Đào Hoàng Sơn <daohoangson@gmail.com>
2021-08-07 15:13:11 +0000

    Add tests

--- packages/fwfh_cached_network_image/lib/src/cached_network_image_factory.dart
+++ packages/fwfh_cached_network_image/lib/src/cached_network_image_factory.dart
@@ -1,9 +1,13 @@
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/widgets.dart';
+import 'package:flutter_cache_manager/flutter_cache_manager.dart';
 import 'package:flutter_widget_from_html_core/flutter_widget_from_html_core.dart';
 
 /// A mixin that can render IMG with `cached_network_image` plugin.
 mixin CachedNetworkImageFactory on WidgetFactory {
+  /// Uses a custom cache manager.
+  BaseCacheManager? get cacheManager => null;
+
   @override
   Widget? buildImageWidget(BuildMetadata meta, ImageSource src) {
     final url = src.url;
@@ -12,6 +16,7 @@ mixin CachedNetworkImageFactory on WidgetFactory {
     }
 
     return CachedNetworkImage(
+      cacheManager: cacheManager,
       errorWidget: (context, _, error) =>
           imageErrorBuilder(context, error, null, src),
       fit: BoxFit.fill,
--- packages/fwfh_cached_network_image/test/cached_network_image_factory_test.dart
+++ packages/fwfh_cached_network_image/test/cached_network_image_factory_test.dart
@@ -3,9 +3,10 @@ import 'package:flutter_test/flutter_test.dart';
 import '_.dart';
 
 void main() {
-  testWidgets('renders IMG tag', (WidgetTester tester) async {
-    final sizingConstraints = 'height≥0.0,height=auto,width≥0.0,width=auto';
-    final src = 'http://domain.com/image.png';
+  final sizingConstraints = 'height≥0.0,height=auto,width≥0.0,width=auto';
+  final src = 'http://domain.com/transparent.gif';
+
+  testWidgets('renders CachedNetworkImage', (WidgetTester tester) async {
     final html = '<img src="$src" />';
     final explained = await explain(tester, html);
     expect(
@@ -14,5 +15,38 @@ void main() {
           '[CachedNetworkImage:imageUrl=$src]'
           ']'),
     );
+  });
+
+  testWidgets('renders Image for data uri', (WidgetTester tester) async {
+    final html = '<img src="$kDataUri" />';
+    final explained = (await explain(tester, html))
+        .replaceAll(RegExp(r'Uint8List#[0-9a-f]+,'), 'bytes,');
+    expect(
+        explained,
+        equals('[CssSizing:$sizingConstraints,child='
+            '[Image:image=MemoryImage(bytes, scale: 1.0)]'
+            ']'));
+  });
+
+  testWidgets('renders progress indicator', (WidgetTester tester) async {
+    final html = '<img src="${src}yolo" />';
+    final explained = await explain(
+      tester,
+      html,
+      useExplainer: false,
+    );
+    expect(explained, contains('CircularProgressIndicator'));
+  });
+
+  testWidgets('renders raw image', (WidgetTester tester) async {
+    final html = '<img src="$src" />';
+    final explained = await explain(tester, html, useExplainer: false);
+    expect(explained, contains('RawImage'));
+  });
+
+  testWidgets('handles error', (WidgetTester tester) async {
+    final html = '<img src="http://domain.com/error.jpg" />';
+    final explained = await explain(tester, html, useExplainer: false);
+    expect(explained, contains('Text("❌")'));
   });
 }
