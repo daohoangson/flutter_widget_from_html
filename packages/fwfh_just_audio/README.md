16c3c215885624a108372c3a7aa353530fc3a917
Dao Hoang Son <daohoangson@gmail.com>
2021-08-11 18:31:20 +0700

    [fwfh_just_audio] v0.6.2

--- packages/fwfh_just_audio/README.md
+++ packages/fwfh_just_audio/README.md
@@ -14,7 +14,7 @@ Add this to your app's `pubspec.yaml` file:
 ```yaml
 dependencies:
   flutter_widget_from_html_core: any
-  fwfh_just_audio: ^0.6.0
+  fwfh_just_audio: ^0.6.2
 ```
 
 ## Usage
--- tool/pub-get.sh
+++ tool/pub-get.sh
@@ -4,12 +4,12 @@ set -e
 
 find . -name 'pubspec.lock' -delete
 
-( cd packages/core && flutter pub get )
-( cd packages/fwfh_cached_network_image && flutter pub get )
-( cd packages/fwfh_chewie && flutter pub get )
-( cd packages/fwfh_just_audio && flutter pub get )
-( cd packages/fwfh_svg && flutter pub get )
-( cd packages/fwfh_url_launcher && flutter pub get )
-( cd packages/fwfh_webview && flutter pub get )
-( cd packages/enhanced && flutter pub get )
-( cd demo_app && flutter pub get )
+(cd packages/core && flutter pub get)
+(cd packages/fwfh_cached_network_image && flutter pub get)
+(cd packages/fwfh_chewie && flutter pub get)
+(cd packages/fwfh_just_audio && flutter pub get)
+(cd packages/fwfh_svg && flutter pub get)
+(cd packages/fwfh_url_launcher && flutter pub get)
+(cd packages/fwfh_webview && flutter pub get)
+(cd packages/enhanced && flutter pub get)
+(cd demo_app && flutter pub get)
