ceb21d0566a6fe4f734c765e52f4e5265f7d5cd5
Đào Hoàng Sơn <daohoangson@gmail.com>
2021-08-12 13:11:06 +0000

    Update devcontainer for Flutter 2.2.2

--- .devcontainer/build.sh
+++ .devcontainer/build.sh
@@ -1,13 +1,17 @@
-#!/bin/sh
+#!/bin/bash
 
 set -euo pipefail
 
-curl https://storage.googleapis.com/flutter_infra/releases/releases_linux.json -Lo /tmp/releases.json
-_baseUrl=$( cat /tmp/releases.json | jq -r .base_url )
-_stableHash=$( cat /tmp/releases.json | jq -r .current_release.stable )
-_stableArchive=$( cat /tmp/releases.json | jq -r ".releases[] | select(.hash == \"$_stableHash\") | .archive" )
+curl https://storage.googleapis.com/flutter_infra_release/releases/releases_linux.json -Lo /tmp/releases.json
+_baseUrl=$(cat /tmp/releases.json | jq -r .base_url)
+_stableHash=$(cat /tmp/releases.json | jq -r .current_release.stable)
+_stableArchive=$(cat /tmp/releases.json | jq -r ".releases[] | select(.hash == \"$_stableHash\") | .archive")
 rm /tmp/releases.json
 
 cd $HOME
-curl $_baseUrl/$_stableArchive -vo /tmp/flutter.tar.xz
+curl "${_baseUrl}/${_stableArchive}" -vo /tmp/flutter.tar.xz
 tar -xf /tmp/flutter.tar.xz
+rm 
+
+flutter precache
+flutter doctor -v
