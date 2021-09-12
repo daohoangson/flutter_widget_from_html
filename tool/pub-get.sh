4a4f180d168ffd34e741be3ec31f9f01f3cb7f19
Dao Hoang Son <daohoangson@gmail.com>
2021-09-12 10:04:55 +0700

    Reset `pubspec.lock` in `tool/pub-get.sh`

--- tool/pub-get.sh
+++ tool/pub-get.sh
@@ -4,12 +4,12 @@ set -e
 
 find . -name 'pubspec.lock' -delete
 
-(cd packages/core && flutter pub get)
-(cd packages/fwfh_cached_network_image && flutter pub get)
-(cd packages/fwfh_chewie && flutter pub get)
-(cd packages/fwfh_just_audio && flutter pub get)
-(cd packages/fwfh_svg && flutter pub get)
-(cd packages/fwfh_url_launcher && flutter pub get)
-(cd packages/fwfh_webview && flutter pub get)
-(cd packages/enhanced && flutter pub get)
-(cd demo_app && flutter pub get)
+(cd packages/core && rm -f pubspec.lock && flutter pub get)
+(cd packages/fwfh_cached_network_image && rm -f pubspec.lock && flutter pub get)
+(cd packages/fwfh_chewie && rm -f pubspec.lock && flutter pub get)
+(cd packages/fwfh_just_audio && rm -f pubspec.lock && flutter pub get)
+(cd packages/fwfh_svg && rm -f pubspec.lock && flutter pub get)
+(cd packages/fwfh_url_launcher && rm -f pubspec.lock && flutter pub get)
+(cd packages/fwfh_webview && rm -f pubspec.lock && flutter pub get)
+(cd packages/enhanced && rm -f pubspec.lock && flutter pub get)
+(cd demo_app && rm -f pubspec.lock && flutter pub get)
