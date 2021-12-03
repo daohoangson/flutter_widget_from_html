04243d735273a4a9eadf99fb5006beeafd1cd297
Dao Hoang Son <daohoangson@gmail.com>
2021-12-03 21:18:42 +0700

    Fix tag_q.dart

--- packages/core/lib/src/internal/ops/tag_q.dart
+++ packages/core/lib/src/internal/ops/tag_q.dart
@@ -8,8 +8,20 @@ class TagQ {
   TagQ(this.wf);
 
   BuildOp get buildOp => BuildOp(
-        onTree: (_, tree) => tree
-          ..prepend(TextBit(tree, '“'))
-          ..append(TextBit(tree, '”')),
+        onTree: (_, tree) {
+          const opening = '“';
+          const closing = '”';
+          final firstParent = tree.first?.parent;
+          final lastParent = tree.last?.parent;
+          if (firstParent == null || lastParent == null) {
+            tree
+              ..prepend(TextBit(tree, opening))
+              ..append(TextBit(tree, closing));
+            return;
+          }
+
+          firstParent.prepend(TextBit(firstParent, opening));
+          lastParent.append(TextBit(lastParent, closing));
+        },
       );
 }
