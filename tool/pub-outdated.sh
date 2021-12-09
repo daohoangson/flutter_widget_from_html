2cafc59b8396fe8e4864fa9509c96a3258915dae
Dao Hoang Son <daohoangson@gmail.com>
2021-12-09 19:26:20 +0700

    Add support for golden_toolkit@0.12

---
+++ tool/pub-outdated.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+set -e
+
+(cd packages/core && pwd && flutter pub outdated || true)
+(cd packages/fwfh_cached_network_image && pwd && flutter pub outdated || true)
+(cd packages/fwfh_chewie && pwd && flutter pub outdated || true)
+(cd packages/fwfh_just_audio && pwd && flutter pub outdated || true)
+(cd packages/fwfh_svg && pwd && flutter pub outdated || true)
+(cd packages/fwfh_text_style && pwd && flutter pub outdated || true)
+(cd packages/fwfh_url_launcher && pwd && flutter pub outdated || true)
+(cd packages/fwfh_webview && pwd && flutter pub outdated || true)
+(cd packages/enhanced && pwd && flutter pub outdated || true)
+(cd demo_app && pwd && flutter pub outdated || true)
