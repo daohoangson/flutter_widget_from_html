daohoangson/flutter_widget_from_html

commit 460dc0b699782b322dc7e6f871b815c79f6cbb7e

Fix min constraints being 100% (#761)
Related to #742

packages/core/lib/src/widgets/css_sizing.dart
@@ -209,19 +209,17 @@ class _RenderCssSizing extends RenderProxyBox {
         min(c.maxHeight, _maxHeight?.clamp(0.0, c.maxHeight) ?? c.maxHeight);
     final maxWidth =
         min(c.maxWidth, _maxWidth?.clamp(0.0, c.maxWidth) ?? c.maxWidth);
-    final minHeight =
-        min(maxHeight, _minHeight?.clamp(0.0, c.maxHeight) ?? c.minHeight);
-    final minWidth =
-        min(maxWidth, _minWidth?.clamp(0.0, c.maxWidth) ?? c.minWidth);
 
-    final effectiveMinHeight =
-        c.hasTightHeight && _minHeight == null ? 0.0 : minHeight;
-    final effectiveMinWidth =
-        c.hasTightWidth && _minWidth == null ? 0.0 : minWidth;
-    final __preferredHeight =
-        _preferredHeight?.clamp(effectiveMinHeight, maxHeight);
-    final __preferredWidth =
-        _preferredWidth?.clamp(effectiveMinWidth, maxWidth);
+    final __minHeight =
+        min(maxHeight, _minHeight?.clamp(0.0, c.maxHeight) ?? c.minHeight);
+    final __minWidth =
+        min(maxWidth, _minWidth?.clamp(0.0, c.maxWidth) ?? c.minWidth);
+    // ignore min value if it's infinite
+    final minHeight = __minHeight.isFinite ? __minHeight : .0;
+    final minWidth = __minWidth.isFinite ? __minWidth : .0;
+
+    final __preferredHeight = _preferredHeight?.clamp(minHeight, maxHeight);
+    final __preferredWidth = _preferredWidth?.clamp(minWidth, maxWidth);
     // ignore preferred value if it's infinite
     final preferredHeight =
         __preferredHeight?.isFinite == true ? __preferredHeight : null;

packages/core/test/style_sizing_test.dart
@@ -246,6 +246,27 @@ void main() {
         ),
       );
     });
+
+    testWidgets('renders 100%', (WidgetTester tester) async {
+      const html = '<div style="min-height: 100%">Foo</div>';
+      final explained = await explain(
+        tester,
+        null,
+        hw: SingleChildScrollView(
+          child: HtmlWidget(
+            html,
+            key: hwKey,
+          ),
+        ),
+      );
+      expect(
+        explained,
+        equals(
+          '[CssSizing:height≥100.0%,width=100.0%,child='
+          '[RichText:(:Foo)]]',
+        ),
+      );
+    });
   });
 
   group('min-width', () {
@@ -303,6 +324,28 @@ void main() {
           '[CssSizing:width≥20.0,width=100.0%,child='
           '[RichText:(:Foo)]'
           ']',
+        ),
+      );
+    });
+
+    testWidgets('renders 100%', (WidgetTester tester) async {
+      const html = '<div style="min-width: 100%">Foo</div>';
+      final explained = await explain(
+        tester,
+        null,
+        hw: SingleChildScrollView(
+          scrollDirection: Axis.horizontal,
+          child: HtmlWidget(
+            html,
+            key: hwKey,
+          ),
+        ),
+      );
+      expect(
+        explained,
+        equals(
+          '[CssSizing:width≥100.0%,width=100.0%,child='
+          '[RichText:(:Foo)]]',
         ),
       );
     });

commit 67f2c4af102553c16625f3113ee6aaa55be19671

Fix `GOLDEN_SKIP` env var (#765)
* Fix sizing calculation bug caused by #761

packages/core/lib/src/widgets/css_sizing.dart
@@ -205,15 +205,19 @@ class _RenderCssSizing extends RenderProxyBox {
   }
 
   BoxConstraints _applyContraints(BoxConstraints c) {
-    final maxHeight =
-        min(c.maxHeight, _maxHeight?.clamp(0.0, c.maxHeight) ?? c.maxHeight);
-    final maxWidth =
-        min(c.maxWidth, _maxWidth?.clamp(0.0, c.maxWidth) ?? c.maxWidth);
+    final maxHeight = _maxHeight?.clamp(0.0, c.maxHeight) ?? c.maxHeight;
+    final maxWidth = _maxWidth?.clamp(0.0, c.maxWidth) ?? c.maxWidth;
 
-    final __minHeight =
-        min(maxHeight, _minHeight?.clamp(0.0, c.maxHeight) ?? c.minHeight);
-    final __minWidth =
-        min(maxWidth, _minWidth?.clamp(0.0, c.maxWidth) ?? c.minWidth);
+    // special treatment for min values: ignore incoming constraint if it's tight
+    final __minHeight = min(
+      maxHeight,
+      _minHeight?.clamp(0.0, c.maxHeight) ??
+          (c.hasTightHeight ? .0 : c.minHeight),
+    );
+    final __minWidth = min(
+      maxWidth,
+      _minWidth?.clamp(0.0, c.maxWidth) ?? (c.hasTightWidth ? .0 : c.minWidth),
+    );
     // ignore min value if it's infinite
     final minHeight = __minHeight.isFinite ? __minHeight : .0;
     final minWidth = __minWidth.isFinite ? __minWidth : .0;

demo_app/test/golden_test.dart
@@ -14,7 +14,7 @@ import '../../packages/fwfh_chewie/test/mock_video_player_platform.dart';
 import '../../packages/fwfh_webview/test/mock_webview_platform.dart';
 
 final goldenSkipEnvVar = Platform.environment['GOLDEN_SKIP'];
-final goldenSkip = goldenSkipEnvVar == ''
+final goldenSkip = goldenSkipEnvVar == null
     ? Platform.isLinux
         ? null
         : 'Linux only'

demo_app/test/smilie_test.dart
@@ -6,7 +6,7 @@ import 'package:golden_toolkit/golden_toolkit.dart';
 
 void main() {
   final goldenSkipEnvVar = Platform.environment['GOLDEN_SKIP'];
-  final goldenSkip = goldenSkipEnvVar == ''
+  final goldenSkip = goldenSkipEnvVar == null
       ? Platform.isLinux
           ? null
           : 'Linux only'

packages/core/test/style_sizing_test.dart
@@ -562,7 +562,7 @@ void main() {
     });
 
     final goldenSkipEnvVar = Platform.environment['GOLDEN_SKIP'];
-    final goldenSkip = goldenSkipEnvVar == ''
+    final goldenSkip = goldenSkipEnvVar == null
         ? Platform.isLinux
             ? null
             : 'Linux only'

packages/core/test/tag_li_test.dart
@@ -868,7 +868,7 @@ Future<void> main() async {
     });
 
     final goldenSkipEnvVar = Platform.environment['GOLDEN_SKIP'];
-    final goldenSkip = goldenSkipEnvVar == ''
+    final goldenSkip = goldenSkipEnvVar == null
         ? Platform.isLinux
             ? null
             : 'Linux only'

packages/core/test/tag_ruby_test.dart
@@ -230,7 +230,7 @@ Future<void> main() async {
     });
 
     final goldenSkipEnvVar = Platform.environment['GOLDEN_SKIP'];
-    final goldenSkip = goldenSkipEnvVar == ''
+    final goldenSkip = goldenSkipEnvVar == null
         ? Platform.isLinux
             ? null
             : 'Linux only'

packages/core/test/tag_table_test.dart
@@ -815,7 +815,7 @@ Future<void> main() async {
     });
 
     final goldenSkipEnvVar = Platform.environment['GOLDEN_SKIP'];
-    final goldenSkip = goldenSkipEnvVar == ''
+    final goldenSkip = goldenSkipEnvVar == null
         ? Platform.isLinux
             ? null
             : 'Linux only'

packages/fwfh_svg/test/svg_factory_test.dart
@@ -195,7 +195,7 @@ Future<void> main() async {
   });
 
   final goldenSkipEnvVar = Platform.environment['GOLDEN_SKIP'];
-  final goldenSkip = goldenSkipEnvVar == ''
+  final goldenSkip = goldenSkipEnvVar == null
       ? Platform.isLinux
           ? null
           : 'Linux only'

packages/fwfh_text_style/test/fwfh_text_style_test.dart
@@ -209,7 +209,7 @@ Future<void> main() async {
   });
 
   final goldenSkipEnvVar = Platform.environment['GOLDEN_SKIP'];
-  final goldenSkip = goldenSkipEnvVar == ''
+  final goldenSkip = goldenSkipEnvVar == null
       ? Platform.isLinux
           ? null
           : 'Linux only'