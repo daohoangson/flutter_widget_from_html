6150d5a6f340bcb5de90bc37e9c35116b18b24d3
Dao Hoang Son <daohoangson@gmail.com>
2022-10-24 01:40:45 +0700

    Enable isSelectable toggle for font size demo

--- demo_app/lib/screens/font_size.dart
+++ demo_app/lib/screens/font_size.dart
@@ -1,4 +1,5 @@
 import 'package:demo_app/widgets/popup_menu.dart';
+import 'package:demo_app/widgets/selection_area.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_widget_from_html/flutter_widget_from_html.dart';
 
@@ -6,27 +7,31 @@ class FontSizeScreen extends StatelessWidget {
   const FontSizeScreen({Key key}) : super(key: key);
 
   @override
-  Widget build(BuildContext context) => Scaffold(
-        appBar: AppBar(
-          title: const Text('FontSizeScreen'),
-          actions: const [
-            PopupMenu(),
-          ],
-        ),
-        body: Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: SingleChildScrollView(
-            child: Column(
-              children: [
-                ColoredBox(
-                  color: Colors.blue,
-                  child: _Panel(),
-                ),
-                ColoredBox(
-                  color: Colors.red,
-                  child: _Slider(),
-                ),
-              ],
+  Widget build(BuildContext context) => ContextualSelectionArea(
+        child: Scaffold(
+          appBar: AppBar(
+            title: const Text('FontSizeScreen'),
+            actions: const [
+              PopupMenu(
+                toggleIsSelectable: true,
+              ),
+            ],
+          ),
+          body: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: SingleChildScrollView(
+              child: Column(
+                children: [
+                  ColoredBox(
+                    color: Colors.blue,
+                    child: _Panel(),
+                  ),
+                  ColoredBox(
+                    color: Colors.red,
+                    child: _Slider(),
+                  ),
+                ],
+              ),
             ),
           ),
         ),
