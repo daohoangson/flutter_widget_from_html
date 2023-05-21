4b2f7f47748dcd1b4e49fc1a9eea3d9e17232937
Dao Hoang Son <daohoangson@gmail.com>
2023-05-21 15:19:04 +0700

    Revert "Explain golden image"
    This reverts commit 1ebe785be803b410f8fc501eb908528d3d03aa0e.

--- packages/core/test/style_sizing_test.dart
+++ packages/core/test/style_sizing_test.dart
@@ -741,9 +741,6 @@ Future<void> main() async {
                         : const Size(200, 250),
                   );
 
-                  final explained = await explainWithoutPumping();
-                  print(explained);
-
                   await screenMatchesGolden(tester, testCase.key);
                 },
                 skip: goldenSkip != null,
@@ -846,7 +843,7 @@ class _Golden extends StatelessWidget {
   Widget build(BuildContext _) => Scaffold(
         body: Padding(
           padding: const EdgeInsets.all(8.0),
-          child: HtmlWidget(html, key: hwKey),
+          child: HtmlWidget(html),
         ),
       );
 }
