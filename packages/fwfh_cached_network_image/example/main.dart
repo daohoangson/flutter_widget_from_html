f1f73045a657f2d7f061fdedf947d0dac2bc1a2f
Dao Hoang Son <daohoangson@gmail.com>
2023-06-02 15:37:06 +0700

    Revert "Update pubspec SDK constraints"
    This reverts commit ddd9fb70a8fea9aa336b7914ba350fd9fcbb21b2.

--- packages/fwfh_cached_network_image/example/main.dart
+++ packages/fwfh_cached_network_image/example/main.dart
@@ -5,7 +5,7 @@ import 'package:fwfh_cached_network_image/fwfh_cached_network_image.dart';
 void main() => runApp(const MyApp());
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
