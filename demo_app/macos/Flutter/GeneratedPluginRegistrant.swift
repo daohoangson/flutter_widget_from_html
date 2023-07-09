ba3956753e3f3ec297688f41087014f6ecca7e94
Dao Hoang Son <daohoangson@gmail.com>
2023-07-09 16:54:19 +0700

    `flutter pub get`

--- demo_app/macos/Flutter/GeneratedPluginRegistrant.swift
+++ demo_app/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -7,16 +7,18 @@ import Foundation
 
 import audio_session
 import just_audio
+import package_info_plus
 import path_provider_foundation
 import sqflite
 import url_launcher_macos
-import wakelock_macos
+import wakelock_plus
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   AudioSessionPlugin.register(with: registry.registrar(forPlugin: "AudioSessionPlugin"))
   JustAudioPlugin.register(with: registry.registrar(forPlugin: "JustAudioPlugin"))
+  FLTPackageInfoPlusPlugin.register(with: registry.registrar(forPlugin: "FLTPackageInfoPlusPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   SqflitePlugin.register(with: registry.registrar(forPlugin: "SqflitePlugin"))
   UrlLauncherPlugin.register(with: registry.registrar(forPlugin: "UrlLauncherPlugin"))
-  WakelockMacosPlugin.register(with: registry.registrar(forPlugin: "WakelockMacosPlugin"))
+  WakelockPlusMacosPlugin.register(with: registry.registrar(forPlugin: "WakelockPlusMacosPlugin"))
 }
