1073263bc8837021f540e8c4c8fe815aa9e3d6be
Đào Hoàng Sơn <daohoangson@gmail.com>
2023-07-12 02:34:03 +0700

    Fix `border-radius` being overwritten by `border` (#966)
    Related to #959

--- packages/core/lib/src/data/css.dart
+++ packages/core/lib/src/data/css.dart
@@ -56,6 +56,7 @@ class CssBorder {
 
   /// Creates a copy of this border with the sides from [other].
   CssBorder copyFrom(CssBorder other) => copyWith(
+        all: other._all,
         bottom: other._bottom,
         inlineEnd: other._inlineEnd,
         inlineStart: other._inlineStart,
@@ -71,6 +72,7 @@ class CssBorder {
   /// Creates a copy of this border but with the given fields
   /// replaced with the new values.
   CssBorder copyWith({
+    CssBorderSide? all,
     CssBorderSide? bottom,
     CssBorderSide? inlineEnd,
     CssBorderSide? inlineStart,
@@ -84,13 +86,16 @@ class CssBorder {
   }) =>
       CssBorder(
         inherit: inherit,
-        all: _all,
-        bottom: CssBorderSide._copyWith(_bottom, bottom),
-        inlineEnd: CssBorderSide._copyWith(_inlineEnd, inlineEnd),
-        inlineStart: CssBorderSide._copyWith(_inlineStart, inlineStart),
-        left: CssBorderSide._copyWith(_left, left),
-        right: CssBorderSide._copyWith(_right, right),
-        top: CssBorderSide._copyWith(_top, top),
+        all: CssBorderSide._copyWith(_all, all),
+        bottom: all != null ? null : CssBorderSide._copyWith(_bottom, bottom),
+        inlineEnd:
+            all != null ? null : CssBorderSide._copyWith(_inlineEnd, inlineEnd),
+        inlineStart: all != null
+            ? null
+            : CssBorderSide._copyWith(_inlineStart, inlineStart),
+        left: all != null ? null : CssBorderSide._copyWith(_left, left),
+        right: all != null ? null : CssBorderSide._copyWith(_right, right),
+        top: all != null ? null : CssBorderSide._copyWith(_top, top),
         radiusBottomLeft: radiusBottomLeft ?? this.radiusBottomLeft,
         radiusBottomRight: radiusBottomRight ?? this.radiusBottomRight,
         radiusTopLeft: radiusTopLeft ?? this.radiusTopLeft,
