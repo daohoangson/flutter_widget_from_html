13b94c58cb7cffa310542189df99ec850f514d59
Dao Hoang Son <daohoangson@gmail.com>
2023-09-11 00:31:50 +0700

    Skip flutter doctor

--- tool/integration.sh
+++ tool/integration.sh
@@ -7,7 +7,7 @@ cd "$(dirname $(dirname ${BASH_SOURCE[0]}))"/demo_app
 if [ ! -z "$JAVA_HOME_17_X64" ]; then
   # switch to Java 17 in GitHub Actions
   export JAVA_HOME=$JAVA_HOME_17_X64
-  flutter doctor -v
+  echo "JAVA_HOME=$JAVA_HOME"
 fi
 
 exec flutter test --reporter expanded integration_test/auto_resize_test.dart
