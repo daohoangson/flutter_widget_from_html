52b85db9782cc4d6f2992befab06cd725215c06c
Dao Hoang Son <daohoangson@gmail.com>
2023-10-17 21:01:20 +0700

    Fix lint warnings

--- packages/core/test/tag_table_test.dart
+++ packages/core/test/tag_table_test.dart
@@ -1,7 +1,6 @@
 import 'dart:io';
 
 import 'package:flutter/material.dart';
-import 'package:flutter/rendering.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:flutter_widget_from_html_core/flutter_widget_from_html_core.dart';
 import 'package:golden_toolkit/golden_toolkit.dart';
