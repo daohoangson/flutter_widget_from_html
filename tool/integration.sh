127fcfaf2324748626e6723ef4d4b33b51153a21
Dao Hoang Son <daohoangson@gmail.com>
2023-12-24 21:55:47 +0700

    Avoid error `./gradlew: No such file or directory`

--- tool/integration.sh
+++ tool/integration.sh
@@ -4,12 +4,6 @@ set -euo pipefail
 
 cd "$(dirname $(dirname ${BASH_SOURCE[0]}))"/demo_app
 
-if [ ! -z "$JAVA_HOME_17_X64" ]; then
-  # switch to Java 17 in GitHub Actions
-  export JAVA_HOME=$JAVA_HOME_17_X64
-  echo "JAVA_HOME=$JAVA_HOME"
-fi
-
 if ! which patrol; then
   dart pub global activate patrol_cli
 fi
