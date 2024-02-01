466988a6cdd41fe2db4675a345d763e40c85ca94
Đào Hoàng Sơn <daohoangson@gmail.com>
2024-02-01 08:45:34 +0700

    Update integration.sh

--- tool/integration.sh
+++ tool/integration.sh
@@ -4,7 +4,9 @@ set -euo pipefail
 
 cd "$(dirname $(dirname ${BASH_SOURCE[0]}))"/demo_app
 
-if [ ! -z "$JAVA_HOME_17_X64" ]; then
+if [ -z ${JAVA_HOME_17_X64+x} ]; then
+  echo "JAVA_HOME is unchanged: $JAVA_HOME"
+else
   # switch to Java 17 in GitHub Actions
   export JAVA_HOME=$JAVA_HOME_17_X64
   echo "JAVA_HOME=$JAVA_HOME"
