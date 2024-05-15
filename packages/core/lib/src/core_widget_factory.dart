373b7b0a0a6434f087f8ed252d020e8045cd0c4a
Dao Hoang Son <daohoangson@gmail.com>
2024-05-15 23:09:28 +0700

    Update TODO related to non-uniform border

--- packages/core/lib/src/core_widget_factory.dart
+++ packages/core/lib/src/core_widget_factory.dart
@@ -171,9 +171,9 @@ class WidgetFactory extends WidgetFactoryResetter with AnchorWidgetFactory {
     if (borderRadius != null) {
       final borderIsUniform = decoration.border?.isUniform ?? true;
       if (borderIsUniform) {
-        // TODO: require uniform color & style when our minimum Flutter version >= 3.10
-        // support for non-uniform border has been improved since this commit
-        // https://github.com/flutter/flutter/commit/5054b6e514a7af91db9859b4eb55d71177d19cfa
+        // TODO: add support for non-uniform border
+        // https://github.com/flutter/flutter/commit/5054b6e
+        // https://pub.dev/packages/non_uniform_border
         decoration = decoration.copyWith(borderRadius: borderRadius);
         clipBehavior = Clip.hardEdge;
       }
