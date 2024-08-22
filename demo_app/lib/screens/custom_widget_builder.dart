f38b6debb40c5fae25ff4d6e939d4fab1b716c71
Dao Hoang Son <daohoangson@gmail.com>
2024-08-22 10:19:29 +0700

    Revert "Replace `carousel_slider` with its fork"
    This reverts commit a471be4c4e2233203c9497b5f6869f03210b98f9.

--- demo_app/lib/screens/custom_widget_builder.dart
+++ demo_app/lib/screens/custom_widget_builder.dart
@@ -1,4 +1,4 @@
-import 'package:carousel_slider_plus/carousel_slider_plus.dart';
+import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_widget_from_html/flutter_widget_from_html.dart';
 
