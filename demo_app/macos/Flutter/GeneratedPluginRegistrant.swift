0fa344e342a9f4a27dc2a24fda23e0a8a16fd01a
Đào Hoàng Sơn <daohoangson@gmail.com>
2024-11-14 16:04:29 +0700

    Prepare v0.15.3 releases (#1368)
    * [fwfh_webview] v0.15.3
    * [enhanced] v0.15.3

--- demo_app/macos/Flutter/GeneratedPluginRegistrant.swift
+++ demo_app/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -10,7 +10,7 @@ import just_audio
 import package_info_plus
 import path_provider_foundation
 import patrol
-import sqflite
+import sqflite_darwin
 import url_launcher_macos
 import video_player_avfoundation
 import wakelock_plus
--- packages/enhanced/README.md
+++ packages/enhanced/README.md
@@ -27,7 +27,7 @@ Add this to your app's `pubspec.yaml` file:
 
 ```yaml
 dependencies:
-  flutter_widget_from_html: ^0.15.2
+  flutter_widget_from_html: ^0.15.3
 ```
 
 ### Platform specific configuration
